ee344fd69077832b9edf228a84d07435ab966104
Sachin <sachin.satyapira60@gmail.com>
2025-06-27 19:34:44 +0530

    Updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,6 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// Move these meta tags to your public/index.html file inside the <head> section.
+// If you want to set meta tags dynamically, use a library like react-helmet in your App component.
