4ba7c49a8c390f5dcd9de3a27d7bf030e4bbae76
Sachin <sachin.satyapira60@gmail.com>
2025-07-01 08:13:00 +0530

    Updated

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Stats from '../components/Stats';
@@ -9,8 +10,41 @@ import Contact from '../components/Contact';
 import FAQ from '../components/FAQ';
 import Footer from '../components/Footer';
 
+declare global {
+  interface Window {
+    fbq?: (...args: any[]) => void;
+    _fbq?: any;
+  }
+}
+
 
 const Index = () => {
+  useEffect(() => {
+    (function(f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function() {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(
+      window,
+      document,
+      "script",
+      "https://connect.facebook.net/en_US/fbevents.js"
+    );
+    window.fbq("init", "1699670274002302");
+    window.fbq("track", "PageView");
+  }, []);
+
   return (
     <div className="min-h-screen">
       <Header />
