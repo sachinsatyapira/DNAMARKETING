ea550db712564762685bbb21a2625c60ba1cae4a
Sachin <sachin.satyapira60@gmail.com>
2025-07-01 08:31:41 +0530

    Updated

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -19,31 +19,31 @@ declare global {
 
 
 const Index = () => {
-  useEffect(() => {
-    (function(f, b, e, v, n, t, s) {
-      if (f.fbq) return;
-      n = f.fbq = function() {
-        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-      };
-      if (!f._fbq) f._fbq = n;
-      n.push = n;
-      n.loaded = !0;
-      n.version = "2.0";
-      n.queue = [];
-      t = b.createElement(e);
-      t.async = !0;
-      t.src = v;
-      s = b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t, s);
-    })(
-      window,
-      document,
-      "script",
-      "https://connect.facebook.net/en_US/fbevents.js"
-    );
-    window.fbq("init", "1699670274002302");
-    window.fbq("track", "PageView");
-  }, []);
+  // useEffect(() => {
+  //   (function(f, b, e, v, n, t, s) {
+  //     if (f.fbq) return;
+  //     n = f.fbq = function() {
+  //       n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+  //     };
+  //     if (!f._fbq) f._fbq = n;
+  //     n.push = n;
+  //     n.loaded = !0;
+  //     n.version = "2.0";
+  //     n.queue = [];
+  //     t = b.createElement(e);
+  //     t.async = !0;
+  //     t.src = v;
+  //     s = b.getElementsByTagName(e)[0];
+  //     s.parentNode.insertBefore(t, s);
+  //   })(
+  //     window,
+  //     document,
+  //     "script",
+  //     "https://connect.facebook.net/en_US/fbevents.js"
+  //   );
+  //   window.fbq("init", "1699670274002302");
+  //   window.fbq("track", "PageView");
+  // }, []);
 
   return (
     <div className="min-h-screen">
